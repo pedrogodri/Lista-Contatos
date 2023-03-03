c592b024d78458d68181c554f2a89502f805ef00
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-03-03 19:41:10 -0300

    feat: criando components, model e services no frontend

--- projeto-contatos/src/app/app.module.ts
+++ projeto-contatos/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListContactsComponent } from './components/list-contacts/list-contacts.component';
+import { AddContactComponent } from './components/add-contact/add-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListContactsComponent,
+    AddContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ projeto-contatos/src/app/components/list-contacts/list-contacts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-contacts',
+  templateUrl: './list-contacts.component.html',
+  styleUrls: ['./list-contacts.component.scss']
+})
+export class ListContactsComponent {
+
+}
